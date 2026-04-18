7f4e10fb7e6bf92fb137e3df0e9e55f525341b70
FelipeDiniz <felipebordonidiniz@gmail.com>
2026-04-18 15:45:11 -0300

    Adding multiple pwd error handling and non-abrut exit

--- AuthenticationUtils.h
+++ AuthenticationUtils.h
@@ -15,16 +15,19 @@ namespace AuthenticationUtils
     /**
      * @brief Perform user registration, prompting messages for
      * username and password (twice) and checking if passwords match.
+     * @param maximumAttempts The maximum number of attempts for the user to
+     * input the same password twice.
      * @return true if registration works, false if not.
      */
-    auto registerUser() -> bool;
+    auto registerUser(uint maximumAttempts) -> bool;
 
     /**
      * @brief Perform user login, prompting messages for
      * username and password and checking if passwords match.
+     * * @param maximumAttempts The maximum number of attempts for the user to input the correct password.
      * @return true if login works, false if not.
      */
-    auto loginUser() -> bool;
+    auto loginUser(uint maximumAttempts) -> bool;
 
     /**
      * @brief Transform a string reference to lower case. A good option
