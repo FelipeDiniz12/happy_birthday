6badd1c467469a8efa34204e305f2911915dd2a8
FelipeDiniz <felipebordonidiniz@gmail.com>
2026-04-18 16:59:10 -0300

    Adding workflow for tests

---
+++ tests/run_tests.sh
@@ -0,0 +1,16 @@
+CWD=`pwd`
+
+TESTS=`find $CWD -type f -name "*.sh"`
+
+for test in $TESTS; do
+    if [[ "$test" == *"test_utils.sh"* ]]; then
+        continue
+    fi
+    if [[ "$test" == *"run_tests.sh"* ]]; then
+        continue
+    fi
+    echo "========================================"
+    echo "Running test: $test"
+    echo "========================================"
+    bash "$test"
+done
